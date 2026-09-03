92872facc5eeb8bb4856b08b7d555e19421b375e
amirirdstI <amirirandoust1137@gmail.com>
2026-09-03 13:32:38 +0330

    Close Telegram bot connection after sending

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,14 +1,14 @@
-from telegram import Bot
+# -*- coding: utf-8 -*-
 
+from telegram import Bot
 from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHANNEL_ID
 
 
 async def send_report(report):
-    bot = Bot(token=TELEGRAM_BOT_TOKEN)
-
-    await bot.send_message(
-        chat_id=TELEGRAM_CHANNEL_ID,
-        text=report,
-        parse_mode="HTML",
-        disable_web_page_preview=True,
-    )
+    async with Bot(token=TELEGRAM_BOT_TOKEN) as bot:
+        await bot.send_message(
+            chat_id=TELEGRAM_CHANNEL_ID,
+            text=report,
+            parse_mode="HTML",
+            disable_web_page_preview=True,
+        )
